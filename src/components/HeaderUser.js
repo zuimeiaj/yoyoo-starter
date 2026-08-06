b1f74508bc60a319ec30ce457e0b3e53f4ae91e2
Jun Yao <2498683974@qq.com>
2026-08-06 16:37:47 +0800

    feat 增加官网

--- src/components/HeaderUser.js
+++ src/components/HeaderUser.js
@@ -7,14 +7,15 @@ import { Menu } from 'antd';
 import IconText from '@/lib/ui/IconText';
 
 export default class HeaderUser extends React.Component {
-  handlelogin = async () => {
-    location.href = 'https://vivw.org/';
+  // 官网首页（public/site/）：相对 PUBLIC_URL，开发 /site/、构建 /yoyoo/site/
+  handlelogin = () => {
+    window.open((process.env.PUBLIC_URL || '') + '/site/', '_blank');
   };
 
   render() {
     return (
       <IconText onClick={this.handlelogin} className={'header_action-item'} icon={'denglu1'}>
-        社区
+        官网
       </IconText>
     );
   }
